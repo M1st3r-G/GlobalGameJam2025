bcb010a4638c04e7cdabdfd58e35cdcd1c1acaaf
KillerLP7 <107851117+KillerLP7@users.noreply.github.com>
2025-01-25 21:56:42 +0100

    Falling Fish Done (Except Game Design, so it doesn't crash our game)

--- Assets/Scripts/Enemy/FallingFishSpawner.cs
+++ Assets/Scripts/Enemy/FallingFishSpawner.cs
@@ -14,11 +14,10 @@ namespace Enemy
         [SerializeField] private float spawnRangeLeft;
         [SerializeField] private float spawnHeight;
         [SerializeField] private float waitDelay;
+        [SerializeField] private int chapters;
+        [SerializeField] private ProgressBar currentProgress;
         [SerializeField] private bool[] checkmark;
-
-        private ProgressBar currentProgress;
-
-        private int chapters;
+        
         private bool m_canSpawn;
         private float m_rndPos;
 
@@ -32,26 +31,39 @@ namespace Enemy
         private void Update()
         {
             m_rndPos = Random.Range(spawnRangeLeft, spawnRangeRight);
+            print($"{m_canSpawn}");
         }
 
         private IEnumerator SpawnFallingFish()
         {
             while (true)
             {
-                Instantiate(fallingFish, gameObject.transform.position, gameObject.transform.rotation);
-                gameObject.transform.position = new Vector3(followCamera.transform.position.x + m_rndPos,
-                    followCamera.transform.position.y + spawnHeight, followCamera.transform.position.z + 10);
-                yield return new WaitForSeconds(shootDelay);
+                if (m_canSpawn)
+                {
+                    yield return new WaitForSeconds(shootDelay);
+                    Instantiate(fallingFish, gameObject.transform.position, gameObject.transform.rotation);
+                    gameObject.transform.position = new Vector3(followCamera.transform.position.x + m_rndPos,
+                        followCamera.transform.position.y + spawnHeight, followCamera.transform.position.z + 10);
+                }
+
+                yield return null;
             }
         }
 
         private IEnumerator SpawnWait()
         {
-
-            for (int i = 1; i < chapters + 1; i++)
+            while (true)
             {
-                yield return new WaitUntil(() => currentProgress.progress >= i / (float)chapters);
-                m_canSpawn = checkmark[i - 1];
+                if (!m_canSpawn)
+                {
+                    for (int i = 1; i < chapters + 1; i++)
+                    {
+                        yield return new WaitUntil(() => currentProgress.progress >= i / (float)chapters);
+                        m_canSpawn = checkmark[i - 1];
+                    }
+                    //Only Problem: Game Design, so that the Game doesn't crahsh, when the Player reaches the Top.
+                }
+                //yield return null;
             }
         }
     }
