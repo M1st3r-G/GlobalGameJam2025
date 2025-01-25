5772be3b032a4602de128b2da179c804a9f760b8
KillerLP7 <107851117+KillerLP7@users.noreply.github.com>
2025-01-25 11:09:48 +0100

    EnemyManager -> ThunderEnemy

--- Assets/Scripts/Enemy/EnemyManager.cs
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -1,16 +1,49 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
     public static EnemyManager instance;
+
+    [SerializeField] private float thunderLifespanAlive;
+    [SerializeField] private float thunderLifespanDead;
     
-    [Header("ThunderCloud")] [SerializeField] GameObject thunderAttack;
+    private GameObject[] thunders;
+    private bool thunderIsOn;
 
     private void Awake()
     {
         instance = this;
+        thunders = GameObject.FindGameObjectsWithTag("Thunder");
+        for (int i = 0; i < thunders.Length; i++)
+        {
+            StartCoroutine(Attack(thunders[i]));
+        }
+    }
+    
+    public void ChangeThunderAttack(GameObject thunder)
+    {
+        thunderIsOn = !thunderIsOn;
+
+        thunder.SetActive(thunderIsOn);
+    }
+    
+    IEnumerator Attack(GameObject thunder)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(thunderLifespanDead);
+            //ChangeThunderAttack(thunder);
+            thunder.SetActive(true);
+            print("Show");
+            yield return new WaitForSeconds(thunderLifespanAlive);
+            //ChangeThunderAttack(thunder);
+            thunder.SetActive(false);
+            print("Hide");
+            yield return null;
+        }
     }
 }
---
+++ Assets/Scripts/Enemy/Thunder.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+public class Thunder : MonoBehaviour
+{
+    private bool canHurtPlayer;
+}
