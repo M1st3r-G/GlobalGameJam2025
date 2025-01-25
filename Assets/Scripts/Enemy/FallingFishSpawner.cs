d74c4a4ba9e3ca54f991bec70c89355fc39b9a35
MisterG <g.haegerbaeumer@gmx.de>
2025-01-25 17:47:33 +0100

    Restucture Enemies and Add Cloud

--- Assets/Scripts/Enemy/FallingFishSpawner.cs
+++ Assets/Scripts/Enemy/FallingFishSpawner.cs
@@ -1,38 +1,39 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
-public class FallingFishSpawner : MonoBehaviour
+namespace Enemy
 {
-    [SerializeField] private Camera camera;
-    [SerializeField] private GameObject fallingFish;
-    [SerializeField] private float shootDelay;
-    [SerializeField] private float spawnRangeRight;
-    [SerializeField] private float spawnRangeLeft;
-    [SerializeField] private float spawnHeight;
+    public class FallingFishSpawner : MonoBehaviour
+    {
+        [FormerlySerializedAs("camera")] [SerializeField] private Camera followCamera;
+        [SerializeField] private GameObject fallingFish;
+        [SerializeField] private float shootDelay;
+        [SerializeField] private float spawnRangeRight;
+        [SerializeField] private float spawnRangeLeft;
+        [SerializeField] private float spawnHeight;
     
-    private float m_rndPos;
+        private float m_rndPos;
 
-    private void Awake()
-    {
-        StartCoroutine(SpawnFallingFish());
-    }
-
-    private void Update()
-    {
-        m_rndPos = Random.Range(spawnRangeLeft, spawnRangeRight);
-    }
-
-    IEnumerator SpawnFallingFish()
-    {
-        while (true)
+        private void Awake()
         {
-            gameObject.transform.position = new Vector3(camera.transform.position.x + m_rndPos, camera.transform.position.y + spawnHeight, camera.transform.position.z + 10);
-            yield return new WaitForSeconds(shootDelay);
-            Instantiate(fallingFish, gameObject.transform.position, gameObject.transform.rotation);
-            yield return null;
+            StartCoroutine(SpawnFallingFish());
+        }
+
+        private void Update()
+        {
+            m_rndPos = Random.Range(spawnRangeLeft, spawnRangeRight);
+        }
+
+        private IEnumerator SpawnFallingFish()
+        {
+            while (true)
+            {
+                Instantiate(fallingFish, gameObject.transform.position, gameObject.transform.rotation);
+                gameObject.transform.position = new Vector3(followCamera.transform.position.x + m_rndPos, followCamera.transform.position.y + spawnHeight, followCamera.transform.position.z + 10);
+                yield return new WaitForSeconds(shootDelay);
+            }
         }
     }
 }
